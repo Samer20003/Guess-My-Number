5a57b08216863a3cf28e00a3209a6dc7f3bdf9b0
Samer <smoor.samara2000@gmail.com>
2024-01-31 14:04:25 +0200

    all add

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+const secertNumber = Math.trunc(Math.random() * 20) + 1; // math.trunc cut of the decimal part *20 +1 for numbers between 1 to 20
+let score = 20;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // if the num is null
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number 💢';
+  }
+  // When player win
+  else if (guess === secertNumber) {
+    document.querySelector('.message').textContent = 'Correct Number👌';
+    document.querySelector('.highscore').textContent = score;
+    document.querySelector('.number').textContent = secertNumber;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  }
+  // When player is to high
+  else if (guess > secertNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High 😑';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose 💥';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  // when palyer is to low
+  else if (guess < secertNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low 😐';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose 💥';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+// Again button
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.highscore').textContent = 0;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
